1574e58cd3be1d63f5b7ae446968c5182d5fc6ee
Sagar Murlidhar Mhaske <86242988+sagarmhaske7088@users.noreply.github.com>
2021-07-30 10:26:15 +0530

    Add files via upload

---
+++ swap1.cpp
@@ -0,0 +1,13 @@
+#include"iostream"
+using namespace std;
+int main()
+{
+int a,b,t;
+cout<<"Enter value for aand b to be swapped";
+cin>>a>>b;
+t=a;
+a=b;
+b=t;
+cout<<"\na="<<a<<" b="<<b;
+return 0;
+}
---
+++ switch.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("\nEnter the choice");
+scanf("%d",&c);
+switch(c)
+{
+case 1:
+
+printf("\n Enter value of a and b");
+c=a+b;
+printf("\n%d",c);
+brea
+
+case 2:
+c=a-b;
+printf("\n enter value of a and b");
+
+}
+return 0;
+}
