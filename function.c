ff19b604cb616193f43abfd6e950c8c57b47dfa0
Sagar Murlidhar Mhaske <86242988+sagarmhaske7088@users.noreply.github.com>
2021-07-30 22:22:12 +0530

    Add files via upload

---
+++ function.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main()
+{
+
+    printf("\n in main method");
+    lonawala();
+    printf("\n Back to main");
+}
+void lowasa()
+{
+  printf("\n In thise bot,eating");
+  printf("\nsnapshaot");
+  
+}
+void lonawala()
+{
+  khandala();
+  
+}
+void sinhgad()
+{
+  printf("\n in sinhgad");
+  khadakwasla();
+  printf("\n Taibute to TM");
+  
+  
+}
+void khadakwasla()
+{
+  printf("\n in khadakwasla");
+  lowasa();
+  printf("\n let eat");
+}
+void khandala()
+{
+sinhgad();
+
+
+}
+
+
+
+
+
+
