3acb9fc8ae2222a3f3f9e1992af9e28ce7f8a4fb
Sagar Murlidhar Mhaske <86242988+sagarmhaske7088@users.noreply.github.com>
2021-08-13 10:28:11 +0530

    Add files via upload

---
+++ goto.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+
+printf("\nWay to Home");
+goto hm;
+
+
+
+printf("\nWay to Class");
+
+printf("\nWay to College");
+hm:
+printf("\nWay to school");
+exit (0);
+return 0;
+}
